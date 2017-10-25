3e7cd53cb45ca926e44ce62c5efa6cda1584d6bd
HirokiTachiyama <hirokitachiyama@gmail.com>
2017-10-25 23:14:11 +0900

    以前のファイルを’削除、デシジョンテーブル生成支援ツールをリビルド

--- src/bwdm/BwdmMain.java
+++ src/bwdm/BwdmMain.java
@@ -4,7 +4,7 @@ public class BwdmMain {
     
     public static void main(String[] args) {
         
-        
+          System.out.println("hoge");  
         
         
     }
---
+++ src/workspace/functionDef.java
@@ -0,0 +1,25 @@
+package workspace;
+
+import com.fujitsu.vdmj.Settings;
+import com.fujitsu.vdmj.VDMPP;
+import com.fujitsu.vdmj.lex.Dialect;
+import com.fujitsu.vdmj.lex.LexTokenReader;
+import com.fujitsu.vdmj.syntax.DefinitionReader;
+import com.fujitsu.vdmj.tc.definitions.TCAccessSpecifier;
+import com.fujitsu.vdmj.tc.definitions.TCDefinition;
+import com.fujitsu.vdmj.tc.definitions.TCExplicitFunctionDefinition;
+import com.fujitsu.vdmj.tc.types.TCFunctionType;
+import java.io.File;
+
+public class functionDef {
+    
+    public static void main(String[] args) {
+
+        
+        LexTokenReader ltr = new LexTokenReader(new File("/Users/ht/Workspace/IntelliJ/BWDM/DTGenerationSupportTool/data/problem.vdmpp"), Dialect.VDM_PP);
+        DefinitionReader dr = new DefinitionReader(ltr);
+        //TCExplicitFunctionDefinition tcefd = new TCExplicitFunctionDefinition(new TCAccessSpecifier());
+        
+    }
+    
+}
